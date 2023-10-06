a6cb183b45dae481ec7e310378db14816483ebdc
lionc <shizijian2010@yahoo.com.hk>
2023-10-06 22:53:08 +0800

    add aws s3 service

--- src/server/api/routers/generate.ts
+++ src/server/api/routers/generate.ts
@@ -1,4 +1,7 @@
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
+import OpenAI from "openai";
+import AWS from "aws-sdk";
 
 import {
   createTRPCRouter,
@@ -6,17 +9,80 @@ import {
   protectedProcedure,
 } from "~/server/api/trpc";
 
+import { env } from "~/env.mjs";
+import { b64Image } from "~/data/b64Image";
+
+const s3 = new AWS.S3({
+  credentials: {
+    accessKeyId: env.ACCESS_KEY_ID,
+    secretAccessKey: env.SECRET_ACCESS_KEY,
+  },
+});
+async function generateIcon(prompt: string): Promise<string | undefined> {
+  if (env.MOCK_DALLE === "true") {
+    return b64Image;
+  } else {
+    const response = await openai.images.generate({
+      prompt: prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+    return response.data[0]?.b64_json;
+  }
+}
+
+const openai = new OpenAI({
+  apiKey: env.OPENAI_API_KEY,
+});
+
 export const generateRouter = createTRPCRouter({
-  generateIcon: publicProcedure
+  generateIcon: protectedProcedure
     .input(
       z.object({
         prompt: z.string(),
       })
     )
-    .mutation(({ ctx, input }) => {
+    .mutation(async ({ ctx, input }) => {
       console.log("we are here", input.prompt);
+      // TODO: verify the user has enough credit
+      const { count } = await ctx.prisma.user.updateMany({
+        where: {
+          id: ctx.session.user.id,
+          credits: {
+            gte: 1,
+          },
+        },
+        data: {
+          credits: {
+            decrement: 1,
+          },
+        },
+      });
+
+      if (count <= 0) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "You do not have enough credits",
+        });
+      }
+
+      // make a fetch request to DALL-E
+      const base64EncodedImage = await generateIcon(input.prompt);
+
+      // TODO: save the images to s3 bucket
+      await s3
+        .putObject({
+          Bucket: env.BUCKET_NAME,
+          Body: Buffer.from(base64EncodedImage!, "base64"),
+          Key: "my-imageedd", //TODO: generate a random id
+          ContentEncoding: "base64",
+          ContentType: "image/jpeg",
+        })
+        .promise();
+
       return {
-        message: "success",
+        imageUrl: base64EncodedImage,
       };
     }),
 });
